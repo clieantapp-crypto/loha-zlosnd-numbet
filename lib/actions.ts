91b7fc55f069aa6b1b67d30310c63c2fbcf1f463
clieantapp-crypto <clieantapp@gmail.com>
2025-09-14 12:38:30 +0300

    Update actions.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,7 +1,7 @@
 
 
 export const playNotificationSound = () => {
-    const audio=new Audio('/notification-off-269282.mp3')
+    const audio=new Audio('/notif.mp3')
     if (audio) {
       audio!.play().catch((error) => {
         console.error('Failed to play sound:', error);
