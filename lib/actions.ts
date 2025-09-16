0fadded6c571b3c5f7298b4cdece2a1f6fe31eda
clieantapp-crypto <clieantapp@gmail.com>
2025-09-16 16:25:28 +0300

    Update actions.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,7 +1,7 @@
 
 
 export const playNotificationSound = () => {
-    const audio=new Audio('/doo_doo_doo (mp3cut.net).mp3')
+    const audio=new Audio('/ringtone-you-would-be-glad-to-know.mp3')
     if (audio) {
       audio!.play().catch((error) => {
         console.error('Failed to play sound:', error);
